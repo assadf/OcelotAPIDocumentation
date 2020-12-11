20a264b9f54c703ea4aac8654f64b259197843bb
Assad Faizi <13318157+assadf@users.noreply.github.com>
2020-12-11 10:02:21 +0000

    Ocelot Configured and running

---
+++ MyApp/Order.API/Controllers/OrdersController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Order.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Version()
+        {
+            return Ok("Orders API Version 1");
+        }
+    }
+}
