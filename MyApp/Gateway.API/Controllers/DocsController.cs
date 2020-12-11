a131bcf7a8659ba0fe50b6bcabca9cfb96c44e54
Assad Faizi <13318157+assadf@users.noreply.github.com>
2020-12-11 14:19:42 +0000

    Update

---
+++ MyApp/Gateway.API/Controllers/DocsController.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json;
+
+namespace Gateway.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DocsController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAsync()
+        {
+            var urlOrder = "http://gateway-api/swagger/docs/v1/orders";
+
+            var client = new HttpClient();
+            var response = await client.GetAsync(urlOrder);
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            //OpenApiDocument openApi = JsonConvert.DeserializeObject<OpenApiDocument>(content);
+
+            return Ok(content);
+        }
+    }
+}
